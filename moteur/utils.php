4b0f9835f20749869134a366f0b3790226d5d545
Arnaud180296 <1802arkabimbingoy@gmail.com>
2025-10-05 00:48:40 +0200

    mise en place de la docstring de la fonction mapIntegerToString().

--- moteur/utils.php
+++ moteur/utils.php
@@ -1,10 +1,18 @@
 <?php
 
-    function mapIntegerToString(callable $callback, $intValue){
+    function mapIntegerToString(callable $callback, $intValue) : string{
+        /**
+        * Se charge de convertir le numero de la categorie sous forme d'entier vers la version string de la categorie
+        * @param array ($callback) represente une methode qui retourne un tableau.
+        * @param int ($intValue) represente le numero à faire correspondre en chaine.
+        * @return string () retourne une chaine de caractère.
+        */
         for ($i=0; $i < count(array_keys($callback())); $i++) { 
             if($i === $intValue){
                 return $callback()[$i];
             }
         }
+
+        return "Une erreur est survenue...";
     }
 ?>
